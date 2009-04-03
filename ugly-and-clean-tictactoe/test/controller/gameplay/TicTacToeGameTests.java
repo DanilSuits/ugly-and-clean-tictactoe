6ffda23773284bce376952f068f7e2234f05df2a
Patrick Wilson Welsh <patrickwilsonwelsh@gmail.com>
2009-04-03 20:11:32 +0000

    Addd a TicTacToeGameTest test method; refined the tests.
    git-svn-id: svn://localhost/testgc/trunk@34 e7318d77-96a9-46c9-8bb1-85483236c0ce

--- ugly-and-clean-tictactoe/test/controller/gameplay/TicTacToeGameTests.java
+++ ugly-and-clean-tictactoe/test/controller/gameplay/TicTacToeGameTests.java
@@ -15,6 +15,25 @@ public class TicTacToeGameTests extends TestCase {
 		assertTrue("there was a winner when there shouldn't be", game.noWinnerYet());
 		
 		fakeStrategy.playerSeriesSizes.put(Board.COMPUTER_PLAYER_MARK, 5);
+		game.updateGameState();
+		
 		assertFalse(game.noWinnerYet());
+		assertTrue(game.computerWon());
 	}
+	
+	public void testThatItTakesFiveForTheHumanPlayerToWin() {
+		StrategyFake fakeStrategy = new StrategyFake();
+		Integer currentSequenceSize = 4;
+		fakeStrategy.playerSeriesSizes.put(Board.HUMAN_PLAYER_MARK, currentSequenceSize);	
+		
+		TicTacToeGame game = new TicTacToeGame(fakeStrategy, new StubView());
+		assertTrue("there was a winner when there shouldn't be", game.noWinnerYet());
+		
+		fakeStrategy.playerSeriesSizes.put(Board.HUMAN_PLAYER_MARK, 5);
+		game.updateGameState();
+		
+		assertFalse(game.noWinnerYet());
+		assertTrue(game.humanPlayerWon());
+	}
+
 }
