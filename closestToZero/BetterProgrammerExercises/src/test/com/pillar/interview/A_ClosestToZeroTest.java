ce9c270a891eb0947658878a0a61de9cda4f9b29
Patrick Wilson Welsh <patrickwilsonwelsh@gmail.com>
2009-05-15 12:08:21 +0000

    Initial import.
    git-svn-id: svn://localhost/testgc/trunk@44 e7318d77-96a9-46c9-8bb1-85483236c0ce

---
+++ closestToZero/BetterProgrammerExercises/src/test/com/pillar/interview/A_ClosestToZeroTest.java
@@ -0,0 +1,31 @@
+package com.pillar.interview;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.pillar.interview.A_ClosestToZero;
+
+
+public class A_ClosestToZeroTest {
+	
+	private A_ClosestToZero closest;
+	
+	
+	
+	@Before
+	public void setUp() {
+		closest = new A_ClosestToZero();
+		
+	}
+	
+	@Test
+	public void testSomething()  {
+		
+		
+
+	}
+
+}
