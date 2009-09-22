e37fbf38ad1e309d2eb7d9c2224ca13978572bcb
Patrick Wilson Welsh <patrickwilsonwelsh@gmail.com>
2009-09-22 18:39:53 +0000

    minor refactorings.
    git-svn-id: svn://localhost/testgc/trunk@58 e7318d77-96a9-46c9-8bb1-85483236c0ce

--- ugly-and-clean-tictactoe/src/model/patternsearching/RandomPositionFinder.java
+++ ugly-and-clean-tictactoe/src/model/patternsearching/RandomPositionFinder.java
@@ -3,6 +3,7 @@ package model.patternsearching;
 import model.gamestate.Board;
 
 public class RandomPositionFinder {
+	private static final int NO_POSITION_AVAILABLE = -1;
 	private static final int MAX_RANDOM_GENERATION_TRIES = 1000;
 	private static final int TEN = 10;
 	private Board board;
@@ -14,35 +15,43 @@ public class RandomPositionFinder {
 	// TODO: Test this against a board nearly full of positions; should return
 	// single opening
 	public int findRandomEmptyPosition() {
-		int candidatePosition = (int) (Math.random() * (Board.MAX_BOARD_SIZE));
-
+		int candidatePosition = getAnyRandomPosition();
 		while (candidatePositionIsNotEmpty(candidatePosition)) {
-			candidatePosition = (int) (Math.random() * (Board.MAX_BOARD_SIZE));
+			candidatePosition = getAnyRandomPosition();
 		}
 
 		return candidatePosition;
 	}
 
+	private int getAnyRandomPosition() {
+		return (int) (Math.random() * (Board.MAX_BOARD_SIZE));
+	}
+
 	private boolean candidatePositionIsNotEmpty(int candidatePosition) {
 		return (board.getPosition(candidatePosition) != Board.EMPTY);
 	}
-
+	
 	public int findRandomEmptyMidBoardPosition() {
 		int result = getStartingResultCandidate();
-
 		int numberOfTries = 0;
 		while (isNotWithinBounds(result) || candidatePositionIsNotEmpty(result)) {
-			if (weHaveRunOutOfTries(numberOfTries))
-				return -1;
-
+			if (weHaveRunOutOfTries(numberOfTries)) return NO_POSITION_AVAILABLE;
 			result = getAnotherRandomPositionWithinRange();
-
 			numberOfTries++;
 		}
 
 		return result;
 	}
-
+	
+	private boolean isNotWithinBounds(int result) {
+		return ((!isWithinMidBoardHorizontalStripe(result)) 
+				|| (!isWithinMidBoardVerticalStripe(result)));
+	}
+	
+	private boolean weHaveRunOutOfTries(int numberOfTries) {
+		return numberOfTries >= MAX_RANDOM_GENERATION_TRIES;
+	}
+	
 	private int getAnotherRandomPositionWithinRange() {
 		int result;
 		int rawNumberWithinLowerBoardRange;
@@ -52,18 +61,14 @@ public class RandomPositionFinder {
 		return result;
 	}
 
+	private int boostRawNumberToMidBoardRange(int rawNumberWithinRange) {
+		return Board.MID_BOARD_LOWER_BOUND + rawNumberWithinRange;
+	}
+	
 	private int getStartingResultCandidate() {
 		int rawNumberWithinLowerBoardRange = getPositionWithinLowerBoardRange();
 		int result = boostRawNumberToMidBoardRange(rawNumberWithinLowerBoardRange);
 		return result;
-	}
-
-	private boolean weHaveRunOutOfTries(int numberOfTries) {
-		return numberOfTries >= MAX_RANDOM_GENERATION_TRIES;
-	}
-
-	private boolean isNotWithinBounds(int result) {
-		return ((!isWithinMidBoardHorizontalStripe(result)) || (!isWithinMidBoardVerticalStripe(result)));
 	}
 
 	private boolean isWithinMidBoardHorizontalStripe(int result) {
@@ -77,10 +82,6 @@ public class RandomPositionFinder {
 		int upperBoundModulusLimit = Board.MID_BOARD_UPPER_BOUND % TEN;
 		boolean isWithinMidBoardVerticalStripe = ((resultModulus > (lowerBoundModulusLimit)) && (resultModulus <= (upperBoundModulusLimit)));
 		return isWithinMidBoardVerticalStripe;
-	}
-
-	private int boostRawNumberToMidBoardRange(int rawNumberWithinRange) {
-		return Board.MID_BOARD_LOWER_BOUND + rawNumberWithinRange;
 	}
 
 	private int getPositionWithinLowerBoardRange() {
